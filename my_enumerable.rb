1b5b0560c52bd0217947411d9bd990b722794fc7
michael-duke <michael.21duke@gmail.com>
2022-10-27 16:41:45 +0300

    Implement filter method and max method

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -11,4 +11,22 @@ module MyEnumerable
         p !nums.empty?
     end
 
+    def filter
+      filtered_nums = []
+      list.each { |num| filtered_nums.push(num) if yield num}
+      p filtered_nums
+    end
+
+    def max
+      max_num = 0
+      list.each {|num| max_num = num > max_num ? num : max_num}
+      p max_num
+    end
+
+    def min
+    end
+    
+    def sort
+    # quick sort/ insertion sort... in progress
+    end
 end
